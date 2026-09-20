53561647c7f06207890a43e26ea8c3d179b7aacb
Rash200522 <shehanrash22@gmail.com>
2026-09-20 12:55:53 +0530

    testing menu function

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include "hospital.h"
 
 int main(void) {
-    printf("\n=== Smart Hospital System Test ===\n");
-    printf("compilation works!\n");
-    printf("Press Enter to exit...\n");
-    getchar();
+    ensureDataFolderExists();
+    initializeSystem();
+
+    int running = 1;
+    while (running) {
+        displayMainMenu();
+        int choice = getValidIntInput(1, 6);
+        switch (choice) {
+            case 1: printf("\n[Registration coming soon]\n"); break;
+            case 2: printf("\n[Priority view coming soon]\n"); break;
+            case 3: printf("\n[Patient list coming soon]\n"); break;
+            case 4: printf("\n[Reports coming soon]\n"); break;
+            case 5: displayWardMenu(); break;
+            case 6:
+                printf("\n[SYSTEM] Exiting...\n");
+                running = 0;
+                break;
+        }
+    }
     return 0;
 }
